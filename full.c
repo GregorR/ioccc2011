2ba80b058d192f0edb1012a3d7858f5a44656dd6
Gregor Richards <Richards@codu.org>
2011-11-14 15:19:42 -0500

    First thoughts.

---
+++ full.c
@@ -0,0 +1,20 @@
+#include <sys/mman.h>
+#define i int
+typedef i (*f)(i*);
+#define J(nm) nm(i**a,i***b,i*c,f*F){ (*(*a)
+e(){ } J(f)); }
+J(a))++; } // a_dd
+J(s))--; } // s_ub
+J(r)++); } // r_ight
+J(l)--); } // l_eft
+J(p)); *f(a); } // p_utchar
+J(g))=f[1](0); } // g_etchar
+J(n)); (*c)++; } // n_op
+t(i*w){ i*o[1]; for(*o=0; (*o)<9; (*o)++)o[*o]=*w; } // t=overflow(3)
+q(i*w){ i*o[9]; t(*w); } // q_overflow(2)
+J(o)); f[2](*(*b--)// o_verflow
+
+main(){
+    i c;
+    char*F,*e;
+    F=mmap(0,9<<25,
