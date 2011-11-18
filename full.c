d8841262c3524ad3012334bd7d5c7f94b5297453
Gregor Richards <Richards@codu.org>
2011-11-18 11:14:09 -0500

    Another method of JITting (and another language, dc this time)

---
+++ full.c
@@ -0,0 +1,138 @@
+#define _GNU_SOURCE
+#ifdef AH
+#include<alloca.h>
+#endif
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#if !NOMMAP
+#include<sys/mman.h>
+#endif
+#define A(x,y,z) O(x,y,z##1)O(x,y,z##2)O(x,y,z##3)
+#define B(x,y) A(x,y,1)A(x,y,2)A(x,y,3)
+#define C(x,y) B(x,y##1)B(x,y##2)B(x,y##3)
+#define D(x) C(x,1)C(x,2)C(x,3)
+#define E(x) D(x##1)D(x##2)D(x##3)
+#define G E(1)E(2)E(3)
+
+#define O(x,y,z) x##y##z,
+int mi[] = {G 0};
+int mo[9<<16];
+
+typedef void (*f)(volatile void *);
+typedef struct { int*a,*t,h,*m; char**s; f*j,*l,r; } *dat;
+
+#define S d->
+#define R(x) ;S r(#x);
+#define L(x,y) S l[x](d)R(y)
+#define F(x) void l##x(volatile dat d) {
+#define OP(x) S a[-1] x##=*S a;S a--;R(x)
+
+void jit(volatile dat d) {
+    R(1)/*R(F)*/
+    OP(+)OP(-)OP(*)
+    *S a=-*S a;R(_)
+    L(1,/)
+    L(2,P)
+    L(3,p)
+    L(4,v)
+#undef O
+#define O(x,y,z) *++S a=S m[(x*S h+y)*S h+z];S r("");
+    G
+    S l[0](d);
+}
+
+void end() {}
+
+int stack[9<<16];
+void *last;
+void *code[9<<16];
+int size[256];
+char *j, *e;
+
+void rsub(int v) {
+    void *a=0,**b=&a;
+    code[v] = last;
+    while (*b < (void*) jit || *b > (void *) end) b++;
+    size[v] = *b - last - size['F'];
+    last = *b;
+}
+
+void recorder(volatile char *rec) {
+    static int i = 0;
+    rsub(*rec?*rec:mi[i++]);
+}
+
+F(d) S a[-1]/=*S a;S a--; }
+F(P) printf("%d",*S a); fflush(stdout); }
+F(p) printf("%d\n",*S a); }
+F(v) *S a=sqrt(*S a); }
+
+void *lib1[] = {end,end,end,end,end};
+void *lib2[] = {end,ld,lP,lp,lv};
+
+#define W(c) memcpy(e,code[c],size[c]);e+=size[c];
+
+void literal(int v) {
+    if (v>256) {
+        literal(v/256);
+        literal(256);
+        W('*');
+    } else {
+        W(mi[v]);
+    }
+}
+
+int main() {
+    int i;
+    char c;
+    dat d = malloc(64);
+    S a = stack;
+    S h = 0;
+    S m = mo;
+    S l = (f*) lib1;
+    S r = (f) recorder;
+    for (i=0;mi[i];i++)mo[mi[i]]=i;
+    last=jit;
+    jit(d);
+    size['1'] -= size['F'];
+    code['2'] = last;
+    size['2'] = 32;
+
+    S h = 100;
+    S l = (f*) lib2;
+    S r = (f) end;
+
+    /* get space */
+    j=e=
+#if NOMMAP
+    malloc(9<<16);
+#else
+    mmap(0,9<<16,-1,MAP_PRIVATE|MAP_ANON,-1,0);
+#endif
+
+    W('1')
+    while (!feof(stdin)) {
+        if (scanf("%d",&i)>0) {
+            literal(i);
+        } else if (scanf("%c",&c)>0) {
+            switch(c){
+                case '_':
+                    scanf("%d",&i);
+                    literal(i);
+                    break;
+            }
+            W(c)
+        } else break;
+    }
+    W('2')
+
+#ifdef JDEBUG
+    fwrite(j, 1, e-j, stdout);
+    fflush(stdout);
+#endif
+    ((f)j)(d);
+
+    return 0;
+}
---
+++ full.try1.c
@@ -0,0 +1,142 @@
+#define _GNU_SOURCE
+#if AH
+#include<alloca.h>
+#endif
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#if !NOMMAP
+#include<sys/mman.h>
+#endif
+#define i int
+#define c char
+#define w while
+#define S switch
+#define T case
+#define U break;
+#define b volatile register
+#define d o->
+#define k d F[3](o);
+#define W 9<<16
+#define J(nm) i nm##s; i*nm(b ar o){
+#define q return(i*)o;
+#define A(x,y,z) O(x,y,z##1)O(x,y,z##2)O(x,y,z##3)
+#define B(x,y) A(x,y,1)A(x,y,2)A(x,y,3)
+#define C(x,y) B(x,y##1)B(x,y##2)B(x,y##3)
+#define D(x) C(x,1)C(x,2)C(x,3)
+#define E(x) D(x##1)D(x##2)D(x##3)
+#define G E(1)E(2)E(3)
+#define O(x,y,z) J(j##x##y##z)d r=(x*d m+y)*d m+z; k} J(J##x##y##z)*d a?(d r=(x*d m+y)*d m+z):0; k} e##x##y##z(){ }
+
+typedef i*(*f)(b void*);
+typedef struct{i*a,r,m;f*F;}*ar;
+
+i*R,Rs,t[W];
+f J[W];
+c*j,*e;
+
+J(a)(*d a)++; q} /* a_dd */
+J(s)(*d a)--; q} /* s_ub */
+J(r)d a++; q} /* r_ight */
+J(l)d a--; q} /* l_eft */
+J(p)d F[0](o); k} /* p_utchar */
+J(g)d F[1](o); k} /* g_etchar */
+J(h)d F[2](o); k} /* JIT ... h */
+u(){ }
+J(m)d F[3](alloca(8))?(d r=9):0; k}
+J(n)alloca(8); }
+
+G
+
+P(b ar o){ putchar(*d a); fflush(stdout); }
+I(b ar o){ *d a=getchar(); }
+v(i*C,ar o){ c a[7]; for(*C=0; *C<6; (*C)++)a[*C]=*d a++; a[6]=0; *C=atoi(a); }
+H(b ar o){
+    i C;
+#define K(f)memcpy(e,(c*)f+ms,f##s);e+=f##s;
+    w((C=*d a++)>=0){
+        S(C){
+            T'+':K(a)U
+            T'-':K(s)U
+            T'>':K(r)U
+            T'<':K(l)U
+            T'.':K(p)U
+            T',':K(g)U
+            T'=':K(h)U
+            T':':K(R)v(&C,o);J[C]=(f)e;memcpy(e,(c*)m,ms);e+=ms;U
+            T'^':v(&C,o);
+#undef O
+#define O(x,y,z) if(C==x##y##z){K(j##x##y##z)}
+            G U
+            T'?':v(&C,o);
+#undef O
+#define O(x,y,z) if(C==x##y##z){K(J##x##y##z)}
+            G U
+        }
+    }
+    K(R)
+}
+
+void*L[]={P,I,H,u};
+
+main(){
+    i*x,C;
+    ar o=malloc(64);
+
+    /* get space */
+    j=e=
+#if NOMMAP
+    malloc(W);
+#else
+    mmap(0,W,-1,MAP_PRIVATE|MAP_ANON,-1,0);
+#endif
+
+    /* figure out the size of the pro/epilogue */
+    if(((c*)m-(c*)u)%4==0&&((c*)s-(c*)a)%4==0){
+        R=(i*)u-1;
+        x=(i*)m-1;
+        w(*++R==*++x);
+        ms=(c*)R-(c*)u;
+        R=(i*)m;
+        x=(i*)n;
+        w(*--R==*--x);
+        Rs=(c*)m-(c*)++R;
+        R=(i*)((c*)R-ms);
+    } else {
+        e=(c*)u-1;
+        w(*++e==0);
+        ms=e-(c*)u;
+        R=(i*)((c*)m-ms-1);
+        C=((c*)m)[-1];
+        Rs=1;
+        e=j;
+    }
+
+    /* figure out the sizes of functions */
+#define M(x,y) x##s=(c*)y-(c*)x-Rs;if(Rs==1)while(((c*)x)[x##s]!=C)x##s--;x##s-=ms;
+    M(a,s)M(s,r)M(r,l)M(l,p)M(p,g)M(g,h)M(h,u)
+#undef O
+#define O(x,y,z) M(j##x##y##z,J##x##y##z)M(J##x##y##z,e##x##y##z)
+    G
+
+    /* init */
+    d a=t;
+    d r=111111;
+    d m=100;
+    d F=(f*)L;
+
+    /* read */
+    w((C=*d a++=getchar())>=0)if(C=='!'){ d a[-1]=-1; U}
+    d a=t;
+
+    /* JIT */
+    H(o);
+#ifdef JDEBUG
+    fprintf(stderr, "%d %d\n", ms, Rs);
+    fwrite(j, 4, 1000, stdout);
+    fflush(stdout);
+#endif
+    w(J[d r]){ J[d r](o); }
+
+    return 0;
+}
